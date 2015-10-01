316a1520e1eca8d69aad894d3bd1fbd6a8d63b94
Steven Barnhurst <stevenbarnhurst@Stevens-MacBook-Pro.local>
2015-10-01 19:12:44 +0900

    init commit

---
+++ js/app.js
@@ -0,0 +1,32 @@
+/**
+ * Created by stevenbarnhurst on 10/1/15.
+ */
+
+(function () {
+
+    'use strict';
+
+    angular.module('catalog', [])
+
+        .controller('CatalogCtrl', ['$scope', '$log', '$http', function($scope, $log, $http) {
+            $scope.getResults = function() {
+                $log.log("test");
+
+                // get the URL from the input
+                var userInput = $scope.input_url;
+                // fire the API request
+                $http.post('/test', {"url": userInput}).
+                    success(function(results) {
+                        $log.log(results);
+                    }).
+                    error(function(error) {
+                        $log.log(error);
+                    });
+            };
+            function menuToggle() {
+                $scope.menu_toggled = !$scope.menu_toggled;
+            }
+
+
+        }]);
+}());
