d9f0d420bdd11c2674c1ed76af86a4c1514f2a76
Steven Barnhurst <stevenbarnhurst@Stevens-MacBook-Pro.local>
2015-10-01 21:15:25 +0900

    edit routes

--- js/app.js
+++ js/app.js
@@ -8,44 +8,20 @@
 
     angular.module('catalog', ['ngroute'])
 
-        .controller('CatalogCtrl', ['$scope', '$log', '$http', function($scope, $log, $http) {
-            $scope.getResults = function() {
-                $log.log("test");
-
-                // get the URL from the input
-                var userInput = $scope.input_url;
-                // fire the API request
-                $http.post('/test', {"url": userInput}).
-                    success(function(results) {
-                        $log.log(results);
-                    }).
-                    error(function(error) {
-                        $log.log(error);
-                    });
-            };
-            function menuToggle() {
-                $scope.menu_toggled = !$scope.menu_toggled;
-            }
-
-
-        }])
         .config(function($routeProvider, $locationProvider) {
             $routeProvider
-                .when('/Book/', {
-                    templateUrl: 'book.html',
-                    controller: 'BookController',
-                    resolve: {
-                        // I will cause a 1 second delay
-                        delay: function ($q, $timeout) {
-                            var delay = $q.defer();
-                            $timeout(delay.resolve, 1000);
-                            return delay.promise;
-                        }
-                    }
+                .when('/', {
+                    templateUrl: 'partials/catalog-items.html',
+                    controller: 'CatalogCtrl'
                 })
-                .when('/Book/:bookId/ch/:chapterId', {
-                    templateUrl: 'chapter.html',
-                    controller: 'ChapterController'
+                .when('/category/:userId', {
+                    templateUrl: 'category-items.html',
+                    controller: 'CategoryCtrl'
+                })
+                .when('/category/:userId/items/:catId', {
+                    templateUrl: 'partials/user-items.html',
+                    controller: 'ItemsCtrl'
                 });
+            $locationProvider.html5Mode(true);
         })
 }());
