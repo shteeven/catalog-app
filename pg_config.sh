16beecbcf1f0bf1c06842ebdd284736d6ac39368
Steven Barnhurst <stevenbarnhurst@Stevens-MacBook-Pro.local>
2015-10-02 13:00:57 +0900

    migrate backend application

---
+++ pg_config.sh
@@ -0,0 +1,16 @@
+#@IgnoreInspection BashAddShebang
+apt-get -qqy update
+apt-get -qqy install postgresql python-psycopg2
+apt-get -qqy install python-flask python-sqlalchemy
+apt-get -qqy install python-pip
+pip install werkzeug==0.8.3
+pip install flask==0.9
+pip install Flask-Login==0.1.3
+pip install oauth2client
+pip install requests
+pip install httplib2
+pip install google-api-python-client
+pip install bleach
+
+vagrantTip="[35m[1mThe shared directory is located at /vagrant\nTo access your shared files: cd /vagrant(B[m"
+echo -e $vagrantTip > /etc/motd
