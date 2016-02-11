023a414e0cb559ca2db1d22d8e73d8c8cf11aba8
Steven Barnhurst <stevenbarnhurst@gmail.com>
2016-02-11 14:41:40 -0700

    change pg_config to run as self contained script

--- pg_config.sh
+++ pg_config.sh
@@ -1,4 +1,4 @@
-#@IgnoreInspection BashAddShebang
+
 apt-get -qqy update
 apt-get -qqy install postgresql python-psycopg2
 apt-get -qqy install python-flask python-sqlalchemy
--- vps/run_vps.sh
+++ vps/run_vps.sh
@@ -3,14 +3,17 @@ config="../pg_config.sh"
 regex="^apt-get|^pip"
 pip install -r $requirements
 
-cat $config | while read line; do
-	if [[ $line =~ $regex ]]; then
-		echo "installing $line"
-		$line
-	else
-		echo "DID NOT INSTALL $line"
-	fi
-done
+chmod +x ${config}
+${config}
+
+#cat $config | while read line; do
+#	if [[ $line =~ $regex ]]; then
+#		echo "installing $line"
+#		$line
+#	else
+#		echo "DID NOT INSTALL $line"
+#	fi
+#done
 
 # move wsgi for use by the VPS
 cp -f catalog-app.wsgi ../
